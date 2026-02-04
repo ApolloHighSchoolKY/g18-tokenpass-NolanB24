e63882adb43fb1169f3cb652a0b24af683e76354
NolanB24 <nolan.bretz@stu.daviess.kyschools.us>
2026-02-04 17:54:30 +0000

    commit

--- TokenPass.java
+++ TokenPass.java
@@ -1,9 +1,18 @@
 public TokenPass(int playerCount){
   /*  to be answered in part (a) */
+  int[] board = new int[playerCount];
+  int currentPlayer = int(Math.random()*(playerCount+1);
+
   
 }
 
 public void distributeCurrentPlayerTokens(){
   /* to be answered in part (b) */
-  
+  for(int i = currentPlayer; currentPlayer < 0; i++)
+  {
+    if(currentPlayer>0)
+      currentPlayer = 0;
+    board[i] += 1;
+      
+  }
 }
